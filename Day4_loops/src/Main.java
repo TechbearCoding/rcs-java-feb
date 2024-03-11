234f560dc1eacd19c5e0dc93ceb1ed425aa629e2
Martins Leitarts <martins.leitarts@kleintech.net>
2024-03-11 19:43:24 +0200

    uzdevums

--- Day4_loops/src/Main.java
+++ Day4_loops/src/Main.java
@@ -1,6 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        sample1();
+        task1();
+    }
+
+    public static void task1(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ievadi vārdu!");
+
+        String input = sc.nextLine();
+        String result = "programmesana";
+
+        if(input.equals(result)){
+            System.out.println("Uzvarēji!");
+        }else if(input.length() == result.length()){
+            System.out.println("Vienāds simbolu skaits");
+        }else{
+            System.out.println("Zaudēji!");
+        }
+
+        if(input.equals(result) || input.length() == result.length()){
+            // && and
+            // || or
+            System.out.println("Uzvarēji!");
+        } else{
+            System.out.println("Zaudēji!");
+        }
+
+
     }
 
     public static void sample1(){
@@ -17,7 +45,9 @@ public class Main {
         String b = "aaa";
 
         if(b.equals("aaa")){
-            
+
+        }else{
+            System.out.println();
         }
     }
 }
