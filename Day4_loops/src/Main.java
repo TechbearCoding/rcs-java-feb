7d5cc826e3e9e47dbe522de6ef21be5e6b4b9ff4
Martins Leitarts <martins.leitarts@kleintech.net>
2024-03-11 21:30:46 +0200

    loop samples

--- Day4_loops/src/Main.java
+++ Day4_loops/src/Main.java
@@ -2,7 +2,38 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        task1();
+        sample3();
+    }
+
+    public static void sample3(){
+        //Uztaisīt programmu, kas ar for ciklu saskaita skaitļus no 1-5
+        int result = 0;
+        for(int i = 1; i < 6; i++){
+            result = result + i;
+        }
+        System.out.println(result);
+    }
+
+    public static void sample2(){
+        //for
+        // i++ ->  i = i+1
+        // i+=2 -> i = i+2
+        for(int i = 0; i < 11; i++){
+            System.out.println(i);
+        }
+    }
+
+    private static void task2() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Sveiki, lūdzu, Ievadiet skaitli:");
+
+        int input = sc.nextInt();
+
+        if((input >= 10) || (input <= 5)){
+            System.out.println("$$");
+        }else {
+            System.out.println("$");
+        }
     }
 
     public static void task1(){
@@ -27,8 +58,6 @@ public class Main {
         } else{
             System.out.println("Zaudēji!");
         }
-
-
     }
 
     public static void sample1(){
