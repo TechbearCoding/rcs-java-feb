b3ea7251e6eed02bc7a546810434d439a6efa55f
Martins Leitarts <martins.leitarts@kleintech.net>
2024-03-11 18:58:34 +0200

    created project

---
+++ Day4_loops/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        sample1();
+    }
+
+    public static void sample1(){
+
+        int a = 4;
+
+        // >
+        // <
+        // >=
+        // <=
+        // ==
+        // !=
+
+        String b = "aaa";
+
+        if(b.equals("aaa")){
+            
+        }
+    }
+}
