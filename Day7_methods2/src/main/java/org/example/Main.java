31c9cce06cb7dea00ea571e9ecb481d504562447
Martins Leitarts <martins.leitarts@kleintech.net>
2024-04-05 21:27:05 +0300

    day 7 task 2

--- Day7_methods2/src/main/java/org/example/Main.java
+++ Day7_methods2/src/main/java/org/example/Main.java
@@ -3,9 +3,21 @@ package org.example;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int result = sample2();
+        int result = task2();
 
         System.out.println(result);
+    }
+
+    public static int task2(){
+        Scanner sc = new Scanner(System.in);
+        int result = 0;
+
+        while(result < 1){
+            System.out.println("Ievadiet skaitli!");
+            result = sc.nextInt();
+        }
+
+        return result;
     }
 
     public static int sample2(){
