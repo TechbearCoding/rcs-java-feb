3052e30bbc51507d6d02d6e056466b4783c0f33a
Martins Leitarts <martins.leitarts@kleintech.net>
2024-04-19 21:28:41 +0300

    samples

--- Day10/src/main/java/org/example/Main.java
+++ Day10/src/main/java/org/example/Main.java
@@ -6,13 +6,58 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        task2();
+        task2c();
+    }
+
+    public static void task2c(){
+        List<Double> skaitli = new ArrayList<>();
+
+        Scanner sc = new Scanner(System.in);
+
+        while(true){
+            System.out.println("Ievadiet skaitli!");
+            skaitli.add(sc.nextDouble());
+            sc.nextLine();
+
+            System.out.println("Exit, lai beigtu ievadi!");
+            String choice = sc.nextLine();
+
+            if(choice.equals("exit")){
+                break;
+            }
+        }
+
+        double videja = getAvg(skaitli);
+        System.out.println(videja);
+    }
+
+    public static void task2b(){
+        List<Double> skaitli = new ArrayList<>();
+
+        Scanner sc = new Scanner(System.in);
+        String choice = "";
+
+        while(!choice.equals("exit")){
+            System.out.println("Ievadiet skaitli!");
+            skaitli.add(sc.nextDouble());
+            sc.nextLine();
+
+            System.out.println("Exit, lai beigtu ievadi!");
+            choice = sc.nextLine();
+        }
+
+        double videja = getAvg(skaitli);
+        System.out.println(videja);
     }
 
     public static void task2(){
         List<Double> skaitli = new ArrayList<>();
 
-        //ievade
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0; i < 6; i++){
+            System.out.println("Ievadiet skaitli!");
+            skaitli.add(sc.nextDouble());
+        }
 
         double videja = getAvg(skaitli);
         System.out.println(videja);
@@ -20,9 +65,11 @@ public class Main {
 
     public static double getAvg(List<Double> sk){
         //dabut videjo vertibu
-        double avg = 0;
-
-        return avg;
+        double sum = 0;
+        for(int i = 0; i < sk.size(); i++){
+            sum += sk.get(i);
+        }
+        return sum / sk.size();
     }
     public static void task1(){
         Scanner sc = new Scanner(System.in);
